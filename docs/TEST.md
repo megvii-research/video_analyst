c6d9eb89ed8092fa98175c6d1dea74d5dbad3f4c
xuyinda <xuyinda@megvii.com>
2020-02-13 18:08:15 +0800

    update docs

---
+++ docs/TEST.md
@@ -0,0 +1,22 @@
+# TEST
+
+A collection of test scripts are located under _tools/_:
+
+- [tools/test_VOT.sh](../tools/test_VOT.sh)
+- [tools/test_GOT.sh](../tools/test_GOT.sh)
+
+## Check test results
+
+_EXP_NAME_ is the string value of key _test.track.exp_name_ in the corresponding _.yaml_ file.
+
+### Check VOT results
+
+```Bash
+cat logs/VOT2018/*.csv
+```
+
+### Check GOT results
+
+```Bash
+view logs/GOT-Benchmark/report/GOT-10k/EXP_NAME/performance.json
+```
--- tools/test_VOT.sh
+++ tools/test_VOT.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_alexnet.yaml'
+python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_alexnet.yaml'
 python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_alexnet-new.yaml'
-# python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_alexnet-multi_temp.yaml'
-# python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_googlenet.yaml'
+python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_alexnet-multi_temp.yaml'
+python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_googlenet.yaml'
 python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_googlenet-new.yaml'
 python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_googlenet_bn.yaml'
-# python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_googlenet-multi_temp.yaml'
-# python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_tinyconv.yaml'
-# python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_tinyconv-multi_temp.yaml'
+python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_googlenet-multi_temp.yaml'
+python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_tinyconv.yaml'
+python3 ./main/test.py --config 'experiments/siamfcpp/test/vot/siamfcpp_tinyconv-multi_temp.yaml'
