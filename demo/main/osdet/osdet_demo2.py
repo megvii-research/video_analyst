03d2f9339f8665d5785312c8fec7b50f04e6bc21
xuyinda <xuyinda@megvii.com>
2020-02-14 22:26:54 +0800

    add one-shot det. demo2

--- demo/main/osdet/osdet_demo2.py
+++ demo/main/osdet/osdet_demo2.py
@@ -12,6 +12,9 @@ from videoanalyst.utils import complete_path_wt_root_in_cfg
 from videoanalyst.pipeline.utils.bbox import xywh2xyxy, xyxy2xywh
 from videoanalyst.pipeline.utils.crop import get_subwindow
 
+from videoanalyst.data.dataset import builder as dataset_buidler
+from videoanalyst.utils.image import load_image
+
 from copy import deepcopy
 import argparse
 import logging
@@ -29,8 +32,10 @@ color = dict(
     search=(255, 255, 0),
     border=(127, 127, 127),
 )
+bbox_thickness = 2
 font_size = 0.5
 font_width = 1
+resize_factor = 0.3
 
 def make_parser():
     parser = argparse.ArgumentParser(description='Test')
@@ -38,14 +43,18 @@ def make_parser():
                         default="experiments/osdet/test/siamfcpp_googlenet-osdet.yaml",
                         type=str,
                         help='experiment configuration')
-    parser.add_argument('--shift_x',
-                        default=0.5,
-                        type=float,
-                        help='crop position x, [0, 1]')
-    parser.add_argument('--shift_y',
-                        default=0.5,
-                        type=float,
-                        help='crop position y, [0, 1]')
+    parser.add_argument('--sequence-index',
+                        default=0,
+                        type=int,
+                        help='template frame index')
+    parser.add_argument('--template-frame',
+                        default=0,
+                        type=int,
+                        help='template frame index')
+    parser.add_argument('--search-frame',
+                        default=1,
+                        type=int,
+                        help='search frame index')
     parser.add_argument('--device',
                         default="cpu",
                         type=str,
@@ -61,41 +70,68 @@ logger.info("Load experiment configuration at: %s" % exp_cfg_path)
 
 # resolve config
 root_cfg = complete_path_wt_root_in_cfg(root_cfg, ROOT_PATH)
-root_cfg = root_cfg.test
-task, task_cfg = specify_task(root_cfg)
+test_cfg = root_cfg.test
+task, task_cfg = specify_task(test_cfg)
 task_cfg.freeze()
 # build model
 model = model_builder.build(task, task_cfg.model)
 # build pipeline
 pipeline = pipeline_builder.build_pipeline(task, task_cfg.pipeline,
                                             model)
+# build dataset
+datasets = dataset_buidler.build(task, root_cfg.train.track.data.sampler.submodules.dataset)
+dataset = datasets[0]
 
 dev = torch.device(parsed_args.device)
 pipeline.to_device(dev)
 
 if __name__ == "__main__":
+
+    seq = dataset[parsed_args.sequence_index]
+
+    template_frame = {k: seq[k][parsed_args.template_frame] for k in seq}
+    template_frame['image'] = load_image(template_frame['image'])
+
+    search_frame = {k: seq[k][parsed_args.search_frame] for k in seq}
+    search_frame['image'] = load_image(search_frame['image'])
+
+    im = template_frame['image']
+    bbox = template_frame['anno']
     rect = xyxy2xywh(bbox)
     pipeline.init(im, rect)
 
-    im_size = np.array((im.shape[1], im.shape[0]), dtype=np.float)
-    crop_pos = np.array([parsed_args.shift_x, parsed_args.shift_y])
-    im_shift = get_subwindow(im, im_size*crop_pos, im_size, im_size)
+    bbox = tuple(map(int, bbox))
+    cv2.rectangle(im, bbox[:2], bbox[2:], color["target"], thickness=bbox_thickness)
+    cv2.rectangle(im, (0, 0), (im.shape[1]-1, im.shape[0]-1) , color["border"], thickness=10)
 
-    rect_pred = pipeline.update(im_shift)
+    im = cv2.resize(im, (0, 0), fx=resize_factor, fy=resize_factor)
+    im = cv2.putText(im, "template frame", (20, 20), cv2.FONT_HERSHEY_COMPLEX, font_size, color["target"], font_width)
+    # cv2.imshow("im", im)
+
+    im_search = search_frame['image']
+    bbox_gt = search_frame['anno']
+    rect_gt = xyxy2xywh(bbox_gt)
+    rect_pred = pipeline.update(im_search)
     bbox_pred = xywh2xyxy(rect_pred)
+
+    bbox_gt = tuple(map(int, bbox_gt))
     bbox_pred = tuple(map(int, bbox_pred))
 
-    im_ = im_shift
-    cv2.rectangle(im_, bbox[:2], bbox[2:], color["target"])
-    cv2.rectangle(im_, bbox_pred[:2], bbox_pred[2:], color["pred"])
-    cv2.rectangle(im_, (0, 0), (im.shape[1]-1, im.shape[0]-1) , color["border"], thickness=10)
+    im_ = im_search
+    cv2.rectangle(im_, bbox_gt[:2], bbox_gt[2:], color["target"], thickness=bbox_thickness)
+    cv2.rectangle(im_, bbox_pred[:2], bbox_pred[2:], color["pred"], thickness=bbox_thickness)
+    cv2.rectangle(im_, (0, 0), (im_.shape[1]-1, im_.shape[0]-1) , color["border"], thickness=10)
 
-    cv2.putText(im_, "original box", (20, 20), cv2.FONT_HERSHEY_COMPLEX, font_size, color["target"], font_width)
-    cv2.putText(im_, "predicted box", (20, 40), cv2.FONT_HERSHEY_COMPLEX, font_size, color["pred"], font_width)
-    cv2.putText(im_, "image border", (20, 60), cv2.FONT_HERSHEY_COMPLEX, font_size, color["border"], font_width)
+    im_ = cv2.resize(im_, (0, 0), fx=resize_factor, fy=resize_factor)
 
+    im_ = cv2.putText(im_, "ground-truth box", (20, 20), cv2.FONT_HERSHEY_COMPLEX, font_size, color["target"], font_width)
+    im_ = cv2.putText(im_, "predicted box", (20, 40), cv2.FONT_HERSHEY_COMPLEX, font_size, color["pred"], font_width)
+    im_ = cv2.putText(im_, "image border", (20, 60), cv2.FONT_HERSHEY_COMPLEX, font_size, color["border"], font_width)
     im_pred = im_
-    cv2.imshow("im_pred", im_pred)
+    # cv2.imshow("im_pred", im_pred)
+
+    im_concat = cv2.vconcat([im, im_pred])
+    cv2.imshow("im_concat", im_concat)
     cv2.waitKey(0)
 
-    from IPython import embed;embed()
+    # from IPython import embed;embed()
