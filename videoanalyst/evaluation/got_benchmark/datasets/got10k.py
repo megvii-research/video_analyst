1c94807069b4237f5d124cb1550a889911a616eb
lizuoxin <lizuoxin@megvii.com>
2020-02-16 17:25:02 +0800

    add cache for got data

--- videoanalyst/evaluation/got_benchmark/datasets/got10k.py
+++ videoanalyst/evaluation/got_benchmark/datasets/got10k.py
@@ -3,8 +3,10 @@ from __future__ import absolute_import, print_function
 import os
 import glob
 import numpy as np
+import pickle
 import six
 
+from tqdm import tqdm
 
 class GOT10k(object):
     r"""`GOT-10K <http://got-10k.aitestunion.com//>`_ Dataset.
@@ -24,25 +26,67 @@ class GOT10k(object):
         list_file (string, optional): If provided, only read sequences
             specified by the file instead of all sequences in the subset.
     """
+    data_dict = {}
     def __init__(self, root_dir, subset='test', return_meta=False,
-                 list_file=None, check_integrity=True):
+                 list_file=None, check_integrity=True, cache_path=None, ignore_cache=False):
         super(GOT10k, self).__init__()
         assert subset in ['train', 'val', 'test'], 'Unknown subset.'
         self.root_dir = root_dir
         self.subset = subset
         self.return_meta = False if subset == 'test' else return_meta
-        
         if list_file is None:
             list_file = os.path.join(root_dir, subset, 'list.txt')
         if check_integrity:
             self._check_integrity(root_dir, subset, list_file)
-
         with open(list_file, 'r') as f:
             self.seq_names = f.read().strip().split('\n')
-        self.seq_dirs = [os.path.join(root_dir, subset, s)
-                         for s in self.seq_names]
-        self.anno_files = [os.path.join(d, 'groundtruth.txt')
-                           for d in self.seq_dirs]
+        if cache_path is None:
+            cache_path = os.path.join(root_dir, subset+".pkl")
+        if GOT10k.data_dict:
+            return 
+        if os.path.isfile(cache_path) and not ignore_cache:
+            print("cache file {} exists".format(cache_path))
+            with open(cache_path, "rb") as f:
+                GOT10k.data_dict = pickle.load(f)
+            print("loaded cache file")
+        else:
+            print("start loading got-10k {}".format(subset))
+            for seq_name in tqdm(self.seq_names):
+                seq_dir = os.path.join(root_dir, subset, seq_name)
+                img_files, anno, meta = self.load_single_sequence(seq_dir)
+                GOT10k.data_dict[seq_name] = dict(img_files = img_files, anno=anno, meta=meta)
+            with open(cache_path, "wb") as f:
+                pickle.dump(GOT10k.data_dict, f)
+            print("dump cache file to {}".format(cache_path))
+
+
+
+    def load_single_sequence(self, seq_dir):
+        r"""        
+        Args:
+            seq_dir (string): path of  a sequence.
+        
+        Returns:
+            tuple: (img_files, anno) if ``return_meta`` is False, otherwise
+                (img_files, anno, meta), where ``img_files`` is a list of
+                file names, ``anno`` is a N x 4 (rectangles) numpy array, while
+                ``meta`` is a dict contains meta information about the sequence.
+        """
+        img_files = sorted(glob.glob(os.path.join(
+            seq_dir, '*.jpg')))
+        anno = np.loadtxt(os.path.join(seq_dir, "groundtruth.txt"), delimiter=',')
+
+        if self.subset == 'test' and anno.ndim == 1:
+            assert len(anno) == 4
+            anno = anno[np.newaxis, :]
+        else:
+            assert len(img_files) == len(anno)
+
+        if self.return_meta:
+            meta = self._fetch_meta(seq_dir)
+            return img_files, anno, meta
+        else:
+            return img_files, anno, None
     
     def __getitem__(self, index):
         r"""        
@@ -55,14 +99,14 @@ class GOT10k(object):
                 file names, ``anno`` is a N x 4 (rectangles) numpy array, while
                 ``meta`` is a dict contains meta information about the sequence.
         """
-        if isinstance(index, six.string_types):
+        if isinstance(index, int):
+            seq_name = self.seq_names[index]
+        else:
             if not index in self.seq_names:
                 raise Exception('Sequence {} not found.'.format(index))
-            index = self.seq_names.index(index)
-
-        img_files = sorted(glob.glob(os.path.join(
-            self.seq_dirs[index], '*.jpg')))
-        anno = np.loadtxt(self.anno_files[index], delimiter=',')
+            seq_name = index
+        img_files = GOT10k.data_dict[seq_name]["img_files"]
+        anno = GOT10k.data_dict[seq_name]["anno"]
 
         if self.subset == 'test' and anno.ndim == 1:
             assert len(anno) == 4
@@ -71,7 +115,7 @@ class GOT10k(object):
             assert len(img_files) == len(anno)
 
         if self.return_meta:
-            meta = self._fetch_meta(self.seq_dirs[index])
+            meta = GOT10k.data_dict[seq_name]["meta"]
             return img_files, anno, meta
         else:
             return img_files, anno
