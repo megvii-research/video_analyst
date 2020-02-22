4e2b7aa3238f29ef4bd2157daa226d1449d52d97
xuyinda <xuyinda@megvii.com>
2020-02-22 12:11:29 +0800

    fix got10k data_dict bug

--- videoanalyst/evaluation/got_benchmark/datasets/got10k.py
+++ videoanalyst/evaluation/got_benchmark/datasets/got10k.py
@@ -8,6 +8,9 @@ import six
 
 from tqdm import tqdm
 
+_VALID_SUBSETS = ['train', 'val', 'test']
+
+
 class GOT10k(object):
     r"""`GOT-10K <http://got-10k.aitestunion.com//>`_ Dataset.
 
@@ -26,52 +29,49 @@ class GOT10k(object):
         list_file (string, optional): If provided, only read sequences
             specified by the file instead of all sequences in the subset.
     """
-    data_dict = {}
+    data_dict = {subset : dict() for subset in _VALID_SUBSETS}
     def __init__(self, root_dir, subset='test', return_meta=False,
                  list_file=None, check_integrity=True, cache_path=None, ignore_cache=False):
         super(GOT10k, self).__init__()
-        assert subset in ['train', 'val', 'test'], 'Unknown subset.'
+        assert subset in _VALID_SUBSETS, 'Unknown subset.'
         self.root_dir = root_dir
         self.subset = subset
         self.return_meta = False if subset == 'test' else return_meta
+
         if list_file is None:
             list_file = os.path.join(root_dir, subset, 'list.txt')
         if check_integrity:
             self._check_integrity(root_dir, subset, list_file)
+
         with open(list_file, 'r') as f:
             self.seq_names = f.read().strip().split('\n')
+
+        # check if subset cache already exists in cls.data_dict
+        if (self.subset in GOT10k.data_dict) and (len(GOT10k.data_dict[self.subset]) > 0):
+            return
+
+        # load subset cache into cls.data_dict
         if cache_path is None:
             cache_path = os.path.join(root_dir, subset+".pkl")
-        if GOT10k.data_dict:
-            return 
         if os.path.isfile(cache_path) and not ignore_cache:
             print("cache file {} exists".format(cache_path))
             with open(cache_path, "rb") as f:
-                GOT10k.data_dict = pickle.load(f)
+                GOT10k.data_dict[self.subset] = pickle.load(f)
             print("loaded cache file {}".format(cache_path))
+        # build subset cache in cls.data_dict and cache to storage
         else:
             print("start loading got-10k {}".format(subset))
             for seq_name in tqdm(self.seq_names):
                 seq_dir = os.path.join(root_dir, subset, seq_name)
                 img_files, anno, meta = self.load_single_sequence(seq_dir)
-                GOT10k.data_dict[seq_name] = dict(img_files = img_files, anno=anno, meta=meta)
+                GOT10k.data_dict[self.subset][seq_name] = dict(img_files = img_files, anno=anno, meta=meta)
             with open(cache_path, "wb") as f:
-                pickle.dump(GOT10k.data_dict, f)
+                pickle.dump(GOT10k.data_dict[self.subset], f)
             print("dump cache file to {}".format(cache_path))
 
 
 
     def load_single_sequence(self, seq_dir):
-        r"""        
-        Args:
-            seq_dir (string): path of  a sequence.
-        
-        Returns:
-            tuple: (img_files, anno) if ``return_meta`` is False, otherwise
-                (img_files, anno, meta), where ``img_files`` is a list of
-                file names, ``anno`` is a N x 4 (rectangles) numpy array, while
-                ``meta`` is a dict contains meta information about the sequence.
-        """
         img_files = sorted(glob.glob(os.path.join(
             seq_dir, '*.jpg')))
         anno = np.loadtxt(os.path.join(seq_dir, "groundtruth.txt"), delimiter=',')
@@ -107,8 +107,8 @@ class GOT10k(object):
                 print(len(self.seq_names))
                 raise Exception('Sequence {} not found.'.format(index))
             seq_name = index
-        img_files = GOT10k.data_dict[seq_name]["img_files"]
-        anno = GOT10k.data_dict[seq_name]["anno"]
+        img_files = GOT10k.data_dict[self.subset][seq_name]["img_files"]
+        anno = GOT10k.data_dict[self.subset][seq_name]["anno"]
 
         if self.subset == 'test' and (anno.size // 4 == 1):
             anno = anno.reshape(-1, 4)
@@ -117,7 +117,7 @@ class GOT10k(object):
             assert len(img_files) == len(anno)
 
         if self.return_meta:
-            meta = GOT10k.data_dict[seq_name]["meta"]
+            meta = GOT10k.data_dict[self.subset][seq_name]["meta"]
             return img_files, anno, meta
         else:
             return img_files, anno
