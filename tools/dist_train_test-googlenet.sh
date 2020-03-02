34ecf0a2891f444d5d067cb8490d8d5d891ca57a
xuyinda <xuyinda@megvii.com>
2020-03-02 13:26:55 +0800

    fix bug in tools/dist

--- tools/dist_train_test-googlenet.sh
+++ tools/dist_train_test-googlenet.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 python3 -W ignore ./main/dist_train.py --config 'experiments/siamfcpp/train/siamfcpp_googlenet-dist_trn.yaml'
-python3 ./main/test.py --config 'experiments/siamfcpp/train/siamfcpp_googlenet-trn.yaml'
+python3 ./main/test.py --config 'experiments/siamfcpp/train/siamfcpp_googlenet-dist_trn.yaml'
