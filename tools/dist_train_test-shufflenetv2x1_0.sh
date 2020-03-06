1b756f127ba8c7b5050cb7fc62df35cb70cfcf60
xuyinda <xuyinda@megvii.com>
2020-03-06 14:29:13 +0800

    fix bug in tools for shufflenet

--- tools/dist_train_test-shufflenetv2x1_0.sh
+++ tools/dist_train_test-shufflenetv2x1_0.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python3 ./main/train.py --config 'experiments/siamfcpp/train/siamfcpp_shufflenetv2x1_0-dist_trn.yaml'
+python3 -W ignore ./main/dist_train.py --config 'experiments/siamfcpp/train/siamfcpp_shufflenetv2x1_0-dist_trn.yaml'
 python3 ./main/test.py --config 'experiments/siamfcpp/train/siamfcpp_shufflenetv2x1_0-dist_trn.yaml'
