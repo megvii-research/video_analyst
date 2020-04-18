464d15031581f542643685e32ef760ebd5485afc
lizuoxin <lizuoxin@megvii.com>
2020-04-18 21:40:30 +0800

    add sat data vis

--- demo/main/debug/visualize_sat_training_data.py
+++ demo/main/debug/visualize_sat_training_data.py
@@ -5,7 +5,7 @@ import argparse
 from loguru import logger
 import os.path as osp
 import pickle
-
+import numpy as np
 import cv2
 
 import torch
@@ -20,7 +20,7 @@ from videoanalyst.model import builder as model_builder
 from videoanalyst.model.loss import builder as losses_builder
 from videoanalyst.optim import builder as optim_builder
 from videoanalyst.pipeline import builder as pipeline_builder
-from videoanalyst.utils import Timer, ensure_dir, complete_path_wt_root_in_cfg
+from videoanalyst.utils import Timer, ensure_dir, complete_path_wt_root_in_cfg, convert_tensor_to_numpy
 
 from videoanalyst.data.utils.visualization import show_img_FCOS
 
@@ -37,16 +37,23 @@ torch.backends.cudnn.deterministic = True
 def make_parser():
     parser = argparse.ArgumentParser(description='Test')
     parser.add_argument(
-        '--config',
+        '-cfg', '--config',
         default='experiments/siamfcpp/data/siamfcpp_data-trn.yaml',
         type=str,
         help='path to experiment configuration')
 
     parser.add_argument(
-        '--target',
-        default='',
+        '-t', '--target',
+        default='dataset',
         type=str,
-        help='targeted debugging module (dataloder|datasampler|dataset))')
+        help='targeted debugging module (datasampler|dataset))')
+
+    parser.add_argument(
+        '-dt',
+        '--data_type',
+        default='bbox',
+        type=str,
+        help='target datat yep(bbox | mask))')
 
     return parser
 
@@ -66,25 +73,8 @@ if __name__ == '__main__':
     root_cfg = root_cfg.train
     task, task_cfg = specify_task(root_cfg)
     task_cfg.data.num_workers = 1
-    task_cfg.data.sampler.submodules.dataset.GOT10kDataset.check_integrity = False
     task_cfg.freeze()
-
-    if parsed_args.target == "dataloader":
-        logger.info("visualize for dataloader")
-        with Timer(name="Dataloader building", verbose=True):
-            dataloader = dataloader_builder.build(task, task_cfg.data)
-
-        for batch_training_data in dataloader:
-            keys = list(batch_training_data.keys())
-            batch_size = len(batch_training_data[keys[0]])
-            training_samples = [{
-                k: v[[idx]]
-                for k, v in batch_training_data.items()
-            } for idx in range(batch_size)]
-            for training_sample in training_samples:
-                target_cfg = task_cfg.data.target
-                show_img_FCOS(target_cfg[target_cfg.name], training_sample)
-    elif parsed_args.target == "dataset":
+    if parsed_args.target == "dataset":
         logger.info("visualize for dataset")
         from videoanalyst.utils import load_image
         import numpy as np
@@ -96,40 +86,44 @@ if __name__ == '__main__':
             seq_idx = np.random.choice(range(len(dataset)))
             seq_idx = int(seq_idx)
             seq = dataset[seq_idx]
-            # video dataset
-            if len(seq['image']) > 1:
+            # for one video
+            if len(seq['image']) > 1: 
                 frame_idx = np.random.choice(range(len(seq['image'])))
                 frame = {k: seq[k][frame_idx] for k in seq}
-                # fetch & visualize data
-                im = load_image(frame['image'])
-                anno = frame['anno']
-            # static image dataset
             else:
-                im = load_image(seq['image'][0])
-                num_anno = len(seq['anno'])
-                if num_anno <= 0:
-                    logger.info("no annotation")
-                    continue
-                anno_idx = np.random.choice(num_anno)
-                anno = seq['anno'][anno_idx]
-            cv2.rectangle(im,
-                          tuple(map(int, anno[:2])),
-                          tuple(map(int, anno[2:])), (0, 255, 0),
-                          thickness=3)
-            im = cv2.resize(im, (0, 0), fx=0.33, fy=0.33)
-            cv2.imshow("im", im)
-            cv2.waitKey(0)
+                frame = dict(image= seq['image'][0],anno=seq['anno'])
+            # fetch & visualize data
+            im = load_image(frame['image'])
+            if parsed_args.data_type == "bbox":
+                cv2.rectangle(im,
+                            tuple(map(int, frame['anno'][:2])),
+                            tuple(map(int, frame['anno'][2:])), (0, 255, 0),
+                            thickness=3)
+                im = cv2.resize(im, (0, 0), fx=0.33, fy=0.33)
+                cv2.imshow("im", im)
+                cv2.waitKey(0)
+            elif parsed_args.data_type == "mask":
+                cv2.imshow("im", im)
+                print(frame['anno'][0])
+                mask = (frame['anno'][0]*50).astype(np.uint8).copy()
+                cv2.imwrite("mask_0.png", mask)
+                cv2.waitKey(0)
+            else:
+                logger.error("data type {} is not support now".format(parsed_args.data_type))
+                exit()
+
     elif parsed_args.target == "datapipeline":
         logger.info("visualize for datapipeline")
         datapipeline = datapipeline_builder.build(task, task_cfg.data, seed=1)
         target_cfg = task_cfg.data.target
-        while True:
+        for i in range(5):
             sampled_data = datapipeline[0]
-            print(sampled_data.keys())
-            show_img_FCOS(
-                target_cfg[target_cfg.name],
-                sampled_data,
-            )
-    else:
-        logger.info("--target {} has not been implemented. ".format(
-            parsed_args.target))
+            print(sampled_data)
+            data = convert_tensor_to_numpy(sampled_data)
+            cv2.imwrite("z.png", data["im_z"].astype(np.uint8))
+            cv2.imwrite("x.png", data["im_x"].astype(np.uint8))
+            cv2.imwrite("g_img.png", data["global_img"].astype(np.uint8))
+            cv2.imwrite("g_mask.png", data["global_mask"].astype(np.uint8)*250)
+            cv2.imwrite("seg_img.png", data["seg_img"].astype(np.uint8))
+            cv2.imwrite("filtered_g.png", data["filtered_global_img"].astype(np.uint8))
+            cv2.imwrite("seg_mask.png", data["seg_mask"].astype(np.uint8)*250)
