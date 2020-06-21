dafc43a795120ecaec03fb60514031b08ff5e6f6
xuyinda <ydxu_zju@hotmail.com>
2020-06-21 15:21:03 +0800

    archive debug_utils for further refinement of desebox_target

---
+++ videoanalyst/data/target/target_impl/utils/debug_utils/compare_tensor.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+tensor_prefix = "center"
+
+tensor_old = np.load("{}_old.npz".format(tensor_prefix), allow_pickle=True)
+tensor_new = np.load("{}_new.npz".format(tensor_prefix), allow_pickle=True)
+
+err = np.abs(tensor_old - tensor_new).max()
+print("{} max_err: {}".format(tensor_prefix, err))
+
+from IPython import embed;embed()
